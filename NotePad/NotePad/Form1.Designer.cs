f8dac85775ca4bad1a78cfe5118206fc5c906cd2
prtsie <yonhsup@mail.ru>
2024-02-15 20:32:52 +0300

    Text edit in files

--- NotePad/NotePad/Form1.Designer.cs
+++ NotePad/NotePad/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NotePad
 {
-    partial class Form1
+    partial class NotepadForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox = new TextBox();
+            menuStrip = new MenuStrip();
+            menuStripFile = new ToolStripMenuItem();
+            menuStripOpenFile = new ToolStripMenuItem();
+            menuStripSaveFile = new ToolStripMenuItem();
+            menuStripSaveFileAs = new ToolStripMenuItem();
+            menuStripView = new ToolStripMenuItem();
+            openFileDialog = new OpenFileDialog();
+            saveFileDialog = new SaveFileDialog();
+            menuStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox
+            // 
+            textBox.AcceptsTab = true;
+            textBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox.Location = new Point(0, 27);
+            textBox.Multiline = true;
+            textBox.Name = "textBox";
+            textBox.ScrollBars = ScrollBars.Vertical;
+            textBox.Size = new Size(800, 423);
+            textBox.TabIndex = 0;
+            textBox.TextChanged += textBox_TextChanged;
+            // 
+            // menuStrip
+            // 
+            menuStrip.Items.AddRange(new ToolStripItem[] { menuStripFile, menuStripView });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(800, 24);
+            menuStrip.TabIndex = 1;
+            menuStrip.Text = "menuStrip1";
+            // 
+            // menuStripFile
+            // 
+            menuStripFile.DropDownItems.AddRange(new ToolStripItem[] { menuStripOpenFile, menuStripSaveFile, menuStripSaveFileAs });
+            menuStripFile.Name = "menuStripFile";
+            menuStripFile.Size = new Size(48, 20);
+            menuStripFile.Text = "Файл";
+            // 
+            // menuStripOpenFile
+            // 
+            menuStripOpenFile.Name = "menuStripOpenFile";
+            menuStripOpenFile.Size = new Size(247, 22);
+            menuStripOpenFile.Text = "Открыть";
+            menuStripOpenFile.Click += menuStripOpenFile_Click;
+            // 
+            // menuStripSaveFile
+            // 
+            menuStripSaveFile.Name = "menuStripSaveFile";
+            menuStripSaveFile.ShortcutKeyDisplayString = "Ctrl + S";
+            menuStripSaveFile.Size = new Size(247, 22);
+            menuStripSaveFile.Text = "Сохранить";
+            menuStripSaveFile.Click += menuStripSaveFile_Click;
+            // 
+            // menuStripSaveFileAs
+            // 
+            menuStripSaveFileAs.Name = "menuStripSaveFileAs";
+            menuStripSaveFileAs.ShortcutKeyDisplayString = "Ctrl + Shift + S";
+            menuStripSaveFileAs.Size = new Size(247, 22);
+            menuStripSaveFileAs.Text = "Сохранить как...";
+            menuStripSaveFileAs.Click += menuStripSaveFileAs_Click;
+            // 
+            // menuStripView
+            // 
+            menuStripView.Name = "menuStripView";
+            menuStripView.Size = new Size(39, 20);
+            menuStripView.Text = "Вид";
+            // 
+            // openFileDialog
+            // 
+            openFileDialog.DefaultExt = "txt";
+            // 
+            // saveFileDialog
+            // 
+            saveFileDialog.DefaultExt = "txt";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox);
+            Controls.Add(menuStrip);
+            MainMenuStrip = menuStrip;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem menuStripFile;
+        private ToolStripMenuItem menuStripSaveFile;
+        private ToolStripMenuItem menuStripSaveFileAs;
+        private ToolStripMenuItem menuStripView;
+        private ToolStripMenuItem menuStripOpenFile;
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
     }
 }
--- NotePad/NotePad/Form1.cs
+++ NotePad/NotePad/Form1.cs
@@ -1,10 +1,97 @@
+using System.Windows.Forms;
+
 namespace NotePad
 {
-    public partial class Form1 : Form
+    public partial class NotepadForm : Form
     {
-        public Form1()
+        private FileInfo? file;
+        private bool isChanged;
+
+        public NotepadForm()
         {
             InitializeComponent();
+            Text = "New";
+            menuStripSaveFile.ShortcutKeys = Keys.Control | Keys.S;
+            menuStripSaveFileAs.ShortcutKeys = Keys.Control | Keys.Shift | Keys.S;
+        }
+
+        private static FileInfo? GetFile(FileDialog dialog)
+        {
+            if (dialog.ShowDialog() == DialogResult.Cancel)
+            {
+                return null;
+            }
+            return new FileInfo(dialog.FileName);
+        }
+
+        private void Save()
+        {
+            if (file is not null)
+            {
+                using var writer = file.CreateText();
+                writer.Write(textBox.Text);
+                isChanged = false;
+            }
+        }
+
+        private void SaveAs()
+        {
+            file = GetFile(saveFileDialog);
+            Save();
+        }
+
+        private static DialogResult SaveOnClose() =>
+            MessageBox.Show("Сохранить файл?", "Сохранение", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+
+        private void menuStripOpenFile_Click(object sender, EventArgs e)
+        {
+            if (isChanged)
+            {
+                switch (SaveOnClose())
+                {
+                    case DialogResult.Cancel:
+                        return;
+                    case DialogResult.Yes:
+                        if (file is not null)
+                        {
+                            Save();
+                        }
+                        else
+                        {
+                            SaveAs();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            file = GetFile(openFileDialog);
+            if (file is not null)
+            {
+                using var reader = file.OpenText();
+                textBox.Text = reader.ReadToEnd();
+                isChanged = false;
+                Text = file.Name;
+            }
+        }
+
+        private void menuStripSaveFile_Click(object sender, EventArgs e)
+        {
+            if (file is not null)
+            {
+                Save();
+            }
+            else
+            {
+                SaveAs();
+            }
+        }
+
+        private void menuStripSaveFileAs_Click(object sender, EventArgs e) => SaveAs();
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            isChanged = true;
         }
     }
 }
